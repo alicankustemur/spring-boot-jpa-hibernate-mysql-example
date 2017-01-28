7b4aecb2e4964cddbab56c5b60f14bc7e8b1e524
alicankustemur <alican.kustemur@gmail.com>
2017-01-28 14:14:20 +0200

    some pretty changes

--- src/main/java/io/github/alicankustemur/person/controller/PersonController.java
+++ src/main/java/io/github/alicankustemur/person/controller/PersonController.java
@@ -1,12 +1,11 @@
 package io.github.alicankustemur.person.controller;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import io.github.alicankustemur.person.domain.Person;
 import io.github.alicankustemur.person.service.PersonService;
@@ -19,7 +18,7 @@ public class PersonController {
 	
 	@RequestMapping("/persons")
 	@ResponseBody
-	public Collection<Person> persons(){
+	public List<Person> persons(){
 		return service.getAllPersons();
 	}
 	
